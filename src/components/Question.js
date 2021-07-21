8021674daf47a86b7ee727b75b538e54f82a16e8
mostafaelhabrok <mostafa.elhabrok@gmail.com>
2021-07-21 18:00:17 +0200

    15

--- src/components/Question.js
+++ src/components/Question.js
@@ -19,7 +19,7 @@ class Question extends Component {
       <div className="Question-holder">
         <div className="Question">{question}</div>
         <div className="Question-status">
-          <img className="category" src={`https://trivia-back-1.herokuapp.com/img/${category}.svg`}/>
+          <img className="category" src={`${category}.svg`}/>
           <div className="difficulty">Difficulty: {difficulty}</div>
           <img src="delete.png" className="delete" onClick={() => this.props.questionAction('DELETE')}/>
           
--- src/components/QuestionView.js
+++ src/components/QuestionView.js
@@ -135,7 +135,7 @@ class QuestionView extends Component {
             Object.keys(this.state.categories).map((id, ) => (
               <li key={id} onClick={() => {this.getByCategory(id)}}>
                 {this.state.categories[id]}
-                <img className="category" src={`https://trivia-back-1.herokuapp.com/img/${this.state.categories[id]}.svg`}/>
+                <img className="category" src={`${this.state.categories[id]}.svg`}/>
               </li>
             ))
             }
